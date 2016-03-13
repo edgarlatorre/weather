b4eea7c620311db31217389f75463941c22cf6b0
Edgar <edgar@me@edgarlatorre.com.br>
2016-03-13 11:56:06 +0000

    Extract unicorn to a variable

--- restart.sh
+++ restart.sh
@@ -1,10 +1,12 @@
+cmd="bin/unicorn"
+
 if [ ! -f tmp/unicorn.pid ]; then
   echo "Staring weather"
-  unicorn -p 3000 -E production -D -c config/unicorn.rb
+  $cmd -p 3000 -E production -D -c config/unicorn.rb
 else
   echo "Restaring weather"
   pid=`cat tmp/unicorn.pid`
   echo "PID: $pid"
   kill $pid
-  unicorn -p 3000 -E production -D -c config/unicorn.rb
+  $cmd -p 3000 -E production -D -c config/unicorn.rb
 fi
